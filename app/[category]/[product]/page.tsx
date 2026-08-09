3b6d0a6e4d4c832acfe040a69071f2b2b666ffb8
emilianoibarra612-debug <emilianoibarra612@gmail.com>
2026-08-09 14:21:16 -0700

    Show owner prices on product pages

--- app/[category]/[product]/page.tsx
+++ app/[category]/[product]/page.tsx
@@ -1,15 +1,4 @@
 import { notFound } from "next/navigation";
-import { getCategory, getProduct } from "../../catalog";
+import { getCategory } from "../../catalog";
 import ProductClient from "./view";
-
-export default async function ProductPage({
-  params,
-}: {
-  params: Promise<{ category: string; product: string }>;
-}) {
-  const { category: categorySlug, product: productSlug } = await params;
-  const category = getCategory(categorySlug);
-  const product = getProduct(categorySlug, productSlug);
-  if (!category || !product) notFound();
-  return <ProductClient category={category} product={product} />;
-}
+export default function ProductPage({params}:{params:{category:string,product:string}}){const category=getCategory(params.category);if(!category)notFound();return <ProductClient category={category} productSlug={params.product}/>}
