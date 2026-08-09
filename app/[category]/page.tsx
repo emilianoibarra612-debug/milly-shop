ebfa2e772c9324a66efe4831434efbd44e0a27d8
emilianoibarra612-debug <emilianoibarra612@gmail.com>
2026-08-09 14:21:00 -0700

    Show added products in categories

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,14 +1,4 @@
 import { notFound } from "next/navigation";
 import { getCategory } from "../catalog";
 import CategoryClient from "./view";
-
-export default async function CategoryPage({
-  params,
-}: {
-  params: Promise<{ category: string }>;
-}) {
-  const { category: categorySlug } = await params;
-  const category = getCategory(categorySlug);
-  if (!category) notFound();
-  return <CategoryClient category={category} />;
-}
+export default function CategoryPage({params}:{params:{category:string}}){const category=getCategory(params.category);if(!category)notFound();return <CategoryClient category={category}/>}
